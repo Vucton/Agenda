431251d9f5aa5b300a086bd6c2b1aff9adbf43d2
Vucton <joovictorrorodri@gmail.com>
2024-02-03 18:39:48 -0300

    Controller $ DAO

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -11,44 +11,52 @@ import javax.servlet.http.HttpServletResponse;
 import model.DAO;
 import model.JavaBeans;
 
-
-@WebServlet(urlPatterns = {"/Controller", "/main", "/insert"})
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
 	JavaBeans contato = new JavaBeans();
-       
-   
-    public Controller() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Controller() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getServletPath();
 		System.out.println(action);
-		if(action.equals("/main")) {
+		if (action.equals("/main")) {
 			contatos(request, response);
-		} else if(action.equals("/insert")) {
+		} else if (action.equals("/insert")) {
 			novoContato(request, response);
-		}else {
+		} else {
 			response.sendRedirect("index.html");
 		}
 	}
-	
-	//Listar contatos:
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	// Listar contatos:
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
-	
-	//Novo Contato:
-		protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			System.out.println(request.getParameter("nome"));
-			System.out.println(request.getParameter("fone"));
-			System.out.println(request.getParameter("email"));
-			
-			//setar as variaveis
-			contato.setNome(request.getParameter("nome"));
-			contato.setFone(request.getParameter("fone"));
-			contato.setEmail(request.getParameter("email"));
-		}
+
+	// Novo Contato:
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/* Verificando se estou recebendo os paramentros
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));*/
+
+		// setar as variaveis
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+
+		// invocar o metodo inserirContato passando o objeto contato
+		dao.inserirContato(contato);
+		
+		//redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
+	}
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -2,22 +2,30 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
-	//Modulo de conexao:
+	/*
+	 * teste de conexao public void testeConexao() {
+	 * 
+	 * try { Connection conn = conectar(); System.out.println(conn); conn.close();
+	 * 
+	 * } catch (Exception e) { System.err.println(e); } }
+	 */
+	// Modulo de conexao:
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "1234";
-	
-	//Metodo de conexão:
+
+	// Metodo de conexão:
 	private Connection conectar() {
 		Connection con = null;
-		
+
 		try {
-			//Vai ler o driver do banco de dados que baixamos
+			// Vai ler o driver do banco de dados que baixamos
 			Class.forName(driver);
-			
+
 			con = DriverManager.getConnection(url, user, password);
 			return con;
 		} catch (Exception e) {
@@ -25,17 +33,25 @@ public class DAO {
 			return null;
 		}
 	}
-	
-	/*teste de conexao
-	public void testeConexao() {
-		
+
+	/* Crud Create */
+	public void inserirContato(JavaBeans contato) {
+		String sql = "INSERT INTO contatos(nome, fone, email) values(?,?,?)";
+
 		try {
-			Connection conn = conectar();
-			System.out.println(conn);
-			conn.close();
-			
+			Connection con = conectar();
+			PreparedStatement pstm = con.prepareStatement(sql);
+
+			pstm.setString(1, contato.getNome());
+			pstm.setString(2, contato.getFone());
+			pstm.setString(3, contato.getEmail());
+
+			pstm.execute();
+			con.close();
 		} catch (Exception e) {
-			System.err.println(e);
+			e.printStackTrace();
 		}
-	}*/
+
+	}
+
 }
